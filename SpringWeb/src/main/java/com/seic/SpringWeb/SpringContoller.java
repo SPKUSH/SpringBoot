6ade6ef0b472b0beb3fdba60f8324d8e64cee4f0
SPKUSH <122848238+SPKUSH@users.noreply.github.com>
2023-01-18 00:03:03 -0500

    Calling next JSP

--- SpringWeb/src/main/java/com/seic/SpringWeb/SpringContoller.java
+++ SpringWeb/src/main/java/com/seic/SpringWeb/SpringContoller.java
@@ -1,8 +1,14 @@
 package com.seic.SpringWeb;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SpringContoller {
@@ -11,5 +17,21 @@ public class SpringContoller {
 	{
 		return("home");
 	}
+	@RequestMapping("/list")
+	public String list()
+	{
+		return("list");
+	}
+	
+	@RequestMapping(value = "/submitForm", method = RequestMethod.POST)
+	public RedirectView submitForm(@RequestParam("mySelect") String val) {
+	    // Do some processing on the form submission
+		RedirectView rv =new RedirectView();
+		rv.setUrl("list");
+		// Redirect to another JSP page
+	    return rv;
+	}
+	
+
 
 }
